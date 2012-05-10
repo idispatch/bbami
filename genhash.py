9ddb0625103c9f17470a9b92ff60958daf10eff6
oleg <oleg@kosenkov.ca>
2012-05-10 14:40:48 -0400

    Implemented attribute name hashing for faster processing

---
+++ genhash.py
@@ -0,0 +1,351 @@
+#!/usr/bin/python3.2
+
+import sys
+import re
+import operator
+
+# http://baldur.iti.kit.edu/~falke/kittel/rta-2011/c/hash.c.txt
+
+def unsigned(x):
+    return x & 0xFFFFFFFF
+    
+def DJBHash(key):
+    hash = 5381
+    for i in key:
+        hash = unsigned(unsigned(unsigned(hash << 5) + hash) + ord(i))
+    return unsigned(hash)
+
+def SDBMHash(key):
+    hash = 0
+    for i in key:
+        hash = unsigned(unsigned(unsigned(ord(i) + unsigned(hash << 6)) + (hash << 16)) - hash)
+    return unsigned(hash)
+
+def LoseHash(key):
+    hash = 0
+    for c in key:
+        hash=unsigned(hash+ord(c))
+    return unsigned(hash)
+
+def RSHash(key):
+    a = 378551
+    b = 63689
+    hash = 0
+    for i in key:
+        hash = unsigned(unsigned(hash * a) + ord(i))
+        a = unsigned(a * b)
+    return unsigned(hash)
+
+def JSHash(key):
+    hash = 1315423911
+    for i in key:
+        hash ^= unsigned(unsigned((unsigned(hash << 5) + ord(i)) + (hash >> 2)))
+    return unsigned(hash)
+
+def PJWHash(key):
+    BitsInUnsignedInt = 32
+    ThreeQuarters = BitsInUnsignedInt * 3 // 4
+    OneEighth = BitsInUnsignedInt // 8
+    HighBits = unsigned((0xFFFFFFFF << (BitsInUnsignedInt - OneEighth)))
+    hash = 0
+    test = 0
+    for i in key:
+        hash = unsigned(unsigned(hash << OneEighth) + ord(i))
+        test = hash & HighBits
+        if test:
+            hash = (( hash ^ (test >> ThreeQuarters)) & (~HighBits))
+    return unsigned(hash)
+
+def ELFHash(key):
+    hash = 0
+    x = 0
+    for i in key:
+        hash = unsigned(unsigned(hash << 4) + ord(i))
+        x = hash & 0xF0000000
+        if x != 0:
+            hash ^= (x >> 24)
+            hash &= ~x
+    return unsigned(hash)
+    
+def FNVHash(key):
+    fnv_prime = 0x811C9DC5
+    hash = 0;
+    for i in key:
+        hash = unsigned(hash * fnv_prime)
+        hash ^= ord(i)
+    return unsigned(hash)
+    
+def BPHash(key):
+    hash = 0;
+    for i in key:
+        hash = unsigned(hash << 7) ^ ord(i)
+    return hash;
+
+def BKDRHash31(key):
+    seed = 31 # 31 131 1313 13131 131313 etc..
+    hash = 0
+    for i in key:
+        hash = unsigned(unsigned(hash * seed) + ord(i))
+    return unsigned(hash)
+
+def BKDRHash131(key):
+        seed = 131 # 31 131 1313 13131 131313 etc..
+        hash = 0
+        for i in key:
+            hash = unsigned(unsigned(hash * seed) + ord(i))
+        return unsigned(hash)
+
+def BKDRHash1313(key):
+        seed = 1313 # 31 131 1313 13131 131313 etc..
+        hash = 0
+        for i in key:
+            hash = unsigned(unsigned(hash * seed) + ord(i))
+        return unsigned(hash)
+
+def BKDRHash13131(key):
+        seed = 13131 # 31 131 1313 13131 131313 etc..
+        hash = 0
+        for i in key:
+            hash = unsigned(unsigned(hash * seed) + ord(i))
+        return unsigned(hash)
+
+def BKDRHash131313(key):
+        seed = 131313 # 31 131 1313 13131 131313 etc..
+        hash = 0
+        for i in key:
+            hash = unsigned(unsigned(hash * seed) + ord(i))
+        return unsigned(hash)
+
+def DEKHash(key):
+    hash = len(key);
+    for i in key:
+        hash = (unsigned(hash << 5) ^ unsigned(hash >>27)) ^ ord(i)
+    return unsigned(hash)
+
+def APHash(key):
+    hash = 0
+    for i in key:
+        if ((ord(i) & 1) == 0):
+            hash ^= (unsigned(hash << 7) ^ ord(i) ^ (hash >> 3))
+        else:
+            hash ^= (~(unsigned(hash << 11) ^ ord(i) ^ (hash >> 5)))
+    return unsigned(hash)
+
+hash_function_header = """
+static unsigned hash_str_impl(const char * str) {"""
+
+hash_function_footer = """}
+"""
+
+hash_functions = {
+    "DJBHash": """
+    unsigned hash = 5381;
+    unsigned c;
+    while((c = *str++)!=0)
+        hash = (hash << 5) + hash + c;
+    return hash;
+""",
+    "SDBMHash": """
+    unsigned hash = 0;
+    unsigned c;
+    while((c = *str++)!=0)
+        hash = c + (hash << 6) + (hash << 16) - hash;
+    return hash;
+""",
+    "LoseHash": """
+    unsigned hash = 0;
+    unsigned c;
+    while((c = *str++)!=0)
+        hash = hash + c;
+    return hash;
+""",
+    "RSHash": """
+    unsigned a = 378551;
+    unsigned b = 63689;
+    unsigned hash = 0;
+    unsigned c;
+    while((c = *str++)!=0)
+        hash = hash * a + c;
+        a = a * b;
+    }
+    return hash;
+""",
+    "JSHash": """
+    unsigned c;
+    unsigned hash = 1315423911;
+    while((c = *str++)!=0)
+        hash ^= (hash << 5) + c + (hash >> 2);
+    return hash;
+""",
+    "PJWHash": """
+    const unsigned BitsInUnsignedInt = (unsigned int)(sizeof(unsigned int) * 8);
+    const unsigned ThreeQuarters     = (unsigned int)((BitsInUnsignedInt  * 3) / 4);
+    const unsigned OneEighth         = (unsigned int)(BitsInUnsignedInt / 8);
+    const unsigned HighBits          = (unsigned int)(0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+    unsigned int hash              = 0;
+    unsigned int test              = 0;
+    unsigned c;
+    while((c = *str++)!=0)
+       hash = (hash << OneEighth) + c;
+       if((test = hash & HighBits)!= 0)
+          hash = (( hash ^ (test >> ThreeQuarters)) & (~HighBits));
+    }
+    return hash;
+""",
+    "ELFHash": """
+    unsigned hash = 0;
+    while(*str) {
+        hash = (hash << 4) + *str++;
+        unsigned g = hash & 0xF0000000;
+        if (g) 
+            hash ^= g >> 24;
+        hash &= ~g;
+    }
+    return hash;
+""",
+    "BKDRHash31": """
+    unsigned seed = 31;
+    unsigned hash = 0;
+    while((c = *str++)!=0)
+        hash = hash * seed + c;
+    return hash;
+""",
+    "BKDRHash131": """
+    unsigned seed = 131;
+    unsigned hash = 0;
+    while((c = *str++)!=0)
+        hash = hash * seed + c;
+    return hash;
+""",   
+    "BKDRHash1313": """
+    unsigned seed = 1313;
+    unsigned hash = 0;
+    while((c = *str++)!=0)
+        hash = hash * seed + c;
+    return hash;
+""",
+    "BKDRHash13131": """
+    unsigned seed = 13131;
+    unsigned hash = 0;
+    while((c = *str++)!=0)
+        hash = hash * seed + c;
+    return hash;
+""",
+    "BKDRHash131313": """
+    unsigned seed = 131313;
+    unsigned hash = 0;
+    while((c = *str++)!=0)
+        hash = hash * seed + c;
+    return hash;
+""",
+    "DEKHash": """
+    unsigned hash = strlen(str);
+    unsigned c;
+    while((c = *str++)!=0)
+        hash = ((hash << 5) ^ (hash >> 27)) ^ c;
+    return hash;
+""",
+    "FNVHash": """
+    const unsigned fnv_prime = 0x811C9DC5;
+    unsigned int hash = 0;
+    unsigned c;
+    while((c = *str++)!=0)
+        hash *= fnv_prime;
+        hash ^= (*str);
+    }
+    return hash;
+""",
+    "BPHash": """
+    unsigned int hash = 0;
+    unsigned c;
+    while((c = *str++)!=0)
+        hash = hash << 7 ^ c;
+    return hash;
+""",
+    "APHash": """
+    unsigned hash = 0;
+    unsigned c;
+    while((c = *str++)!=0) {
+        if (c & 1 == 0)
+            hash ^= (hash << 7) ^ c ^ (hash >> 3);
+        else
+            hash ^= ~((hash << 11) ^ c ^ (hash >> 5));
+    }
+    return hash;
+"""
+}
+
+if len(sys.argv) < 2:
+    sys.exit(-1)
+
+with open(sys.argv[1]) as f:
+    h={}
+    for i in f.readlines():
+        m=re.search(r'\[([^]]+)\]\s*=\s*"([^"]+)"',i)
+        if m:
+            id, value = m.group(1,2)
+            h[id] = value
+
+    #print("Number of strings: %s\n" % len(h))
+    stat = {}
+    for name in hash_functions:  # vary hash algorithm
+        for table_size in range(len(h), 10*len(h)): # vary hash table size
+            table = [0] * table_size
+            fn = globals()[name]
+            for i in h:
+                hash = fn(h[i]) % table_size
+                table[hash] += 1
+            num_collisions=0
+            max_collisions=0
+            for i,collisions in enumerate(table):
+                if collisions > 1:
+                    num_collisions += collisions - 1
+                    max_collisions = max(max_collisions, collisions)
+            if num_collisions == 0:
+                stat[name] = table_size
+                break
+
+    sorted_stat = list(sorted(stat.items(), key = operator.itemgetter(1)))
+    #for entry in sorted_stat:
+    #    print("%-20s: %s" % (entry[0], entry[1]))
+
+    name, table_size = sorted_stat[0]
+    #print("\nUsing %s" % name)
+    
+    table = {}
+    fn = globals()[name]
+    for i in h:
+        hash = fn(h[i]) % table_size
+        table[hash] = i
+    
+    print("static const unsigned attr_hash_table[] = {")
+    for i in range(table_size):
+        if i in table:
+            print('    %-38s, /* %-8s%s */' % (table[i], i, h[table[i]]))
+        else:
+            print("    %s," % "~0")
+    print("};")
+    print("")
+    print("#define HASH_TABLE_SIZE (sizeof(attr_hash_table) / sizeof(attr_hash_table[0]))")
+    print(hash_function_header, end="")
+    print(hash_functions[name], end="")
+    print(hash_function_footer, end="")
+    print("""
+static unsigned hash_str(const char * str) {
+    return hash_str_impl(str) % HASH_TABLE_SIZE;
+}
+
+static int hash_lookup(const char * str, bbami_attribute_id * attribute_id) {
+    if(!str || !attribute_id)
+        return EINVAL;
+    if(!*str)
+        return ENOENT;
+    unsigned hash = hash_str(str);
+    if (hash != ~0 && 
+        0 == strcmp(str, attr_names[attr_hash_table[hash]])) {
+        *attribute_id = (bbami_attribute_id)attr_hash_table[hash];
+        return EOK;
+    }
+    return ENOENT;
+}
+""")
--- main.c
+++ main.c
@@ -5,47 +5,50 @@
 #include "bbami.h"
 
 #define MAX_LINE_LENGTH 2048
+#define USE_HASH_TABLE
 
-static const struct attr_name {
-    const char * attr_name;
-} attr_names[] = {
-    [BBAMI_AUTHOR] = { .attr_name = "Author" },
-    [BBAMI_ARCHIVE_CREATED_BY] = { .attr_name = "Archive-Created-By" },
-    [BBAMI_ARCHIVE_MANIFEST_VERSION] = { .attr_name = "Archive-Manifest-Version" },
+static const char * attr_names[] = {
+    [BBAMI_AUTHOR] = "Author",
+    [BBAMI_ARCHIVE_CREATED_BY] = "Archive-Created-By",
+    [BBAMI_ARCHIVE_MANIFEST_VERSION] = "Archive-Manifest-Version",
 
-    [BBAMI_APPLICATION_NAME] = { .attr_name = "Application-Name" },
-    [BBAMI_APPLICATION_DESCRIPTION] = { .attr_name = "Application-Description" },
-    [BBAMI_APPLICATION_CATEGORY] = { .attr_name = "Application-Category" },
-    [BBAMI_APPLICATION_VERSION] = { .attr_name = "Application-Version" },
-    [BBAMI_APPLICATION_ID] = { .attr_name = "Application-Id" },
-    [BBAMI_APPLICATION_VERSION_ID] = { .attr_name = "Application-Version-Id" },
-    [BBAMI_APPLICATION_DEVELOPMENT_MODE] = { .attr_name = "Application-Development-Mode" },
-    [BBAMI_APPLICATION_REQUIRES_SYSTEM] = { .attr_name = "Application-Requires-System" },
+    [BBAMI_APPLICATION_NAME] = "Application-Name",
+    [BBAMI_APPLICATION_DESCRIPTION] = "Application-Description",
+    [BBAMI_APPLICATION_CATEGORY] = "Application-Category",
+    [BBAMI_APPLICATION_VERSION] = "Application-Version",
+    [BBAMI_APPLICATION_ID] = "Application-Id",
+    [BBAMI_APPLICATION_VERSION_ID] = "Application-Version-Id",
+    [BBAMI_APPLICATION_DEVELOPMENT_MODE] = "Application-Development-Mode",
+    [BBAMI_APPLICATION_REQUIRES_SYSTEM] = "Application-Requires-System",
 
-    [BBAMI_PACKAGE_TYPE] = { .attr_name = "Package-Type" },
-    [BBAMI_PACKAGE_AUTHOR] = { .attr_name = "Package-Author" },
-    [BBAMI_PACKAGE_NAME] = { .attr_name = "Package-Name" },
-    [BBAMI_PACKAGE_VERSION] = { .attr_name = "Package-Version" },
-    [BBAMI_PACKAGE_ARCHITECTURE] = { .attr_name = "Package-Architecture" },
-    [BBAMI_PACKAGE_AUTHOR_CERTIFICATE_HASH] = { .attr_name = "Package-Author-Certificate-Hash" },
-    [BBAMI_PACKAGE_AUTHOR_ID] = { .attr_name = "Package-Author-Id" },
-    [BBAMI_PACKAGE_ID] = { .attr_name = "Package-Id" },
-    [BBAMI_PACKAGE_VERSION_ID] = { .attr_name = "Package-Version-Id" },
+    [BBAMI_PACKAGE_TYPE] = "Package-Type",
+    [BBAMI_PACKAGE_AUTHOR] = "Package-Author",
+    [BBAMI_PACKAGE_NAME] = "Package-Name",
+    [BBAMI_PACKAGE_VERSION] = "Package-Version",
+    [BBAMI_PACKAGE_ARCHITECTURE] = "Package-Architecture",
+    [BBAMI_PACKAGE_AUTHOR_CERTIFICATE_HASH] = "Package-Author-Certificate-Hash",
+    [BBAMI_PACKAGE_AUTHOR_ID] = "Package-Author-Id",
+    [BBAMI_PACKAGE_ID] = "Package-Id",
+    [BBAMI_PACKAGE_VERSION_ID] = "Package-Version-Id",
 
-    [BBAMI_ENTRY_POINT] = { .attr_name = "Entry-Point" },
-    [BBAMI_ENTRY_POINT_NAME] = { .attr_name = "Entry-Point-Name" },
-    [BBAMI_ENTRY_POINT_TYPE] = { .attr_name = "Entry-Point-Type" },
-    [BBAMI_ENTRY_POINT_ICON] = { .attr_name = "Entry-Point-Icon" },
-    [BBAMI_ENTRY_POINT_SPLASH_SCREEN] = { .attr_name = "Entry-Point-Splash-Screen" },
-    [BBAMI_ENTRY_POINT_ORIENTATION] = { .attr_name = "Entry-Point-Orientation" },
-    [BBAMI_ENTRY_POINT_USER_ACTIONS] = { .attr_name = "Entry-Point-User-Actions" },
-    [BBAMI_ENTRY_POINT_SYSTEM_ACTIONS] = { .attr_name = "Entry-Point-System-Actions" },
+    [BBAMI_ENTRY_POINT] = "Entry-Point",
+    [BBAMI_ENTRY_POINT_NAME] = "Entry-Point-Name",
+    [BBAMI_ENTRY_POINT_TYPE] = "Entry-Point-Type",
+    [BBAMI_ENTRY_POINT_ICON] = "Entry-Point-Icon",
+    [BBAMI_ENTRY_POINT_SPLASH_SCREEN] = "Entry-Point-Splash-Screen",
+    [BBAMI_ENTRY_POINT_ORIENTATION] = "Entry-Point-Orientation",
+    [BBAMI_ENTRY_POINT_USER_ACTIONS] = "Entry-Point-User-Actions",
+    [BBAMI_ENTRY_POINT_SYSTEM_ACTIONS] = "Entry-Point-System-Actions",
 
-    [BBAMI_ARCHIVE_ASSET_NAME] = { .attr_name = "Archive-Asset-Name" },
-    [BBAMI_ARCHIVE_ASSET_SHA_512_DIGEST] = { .attr_name = "Archive-Asset-SHA-512-Digest" },
+    [BBAMI_ARCHIVE_ASSET_NAME] = "Archive-Asset-Name",
+    [BBAMI_ARCHIVE_ASSET_SHA_512_DIGEST] = "Archive-Asset-SHA-512-Digest",
 };
 
 #define NUM_ATTRIBUTES (sizeof(attr_names)/sizeof(attr_names[0]))
+
+#ifdef USE_HASH_TABLE
+#include "hash.h"
+#endif
 
 struct hash_entry {
     int count; /* Union type - single value or multiple values
@@ -96,85 +99,92 @@ static int read_manifest_file(bbami_info_ptr info, const char * filename) {
 
         /* Chomp */
         while (NULL != (column = strrchr(line, '\n'))) {
-        	*column = '\0';
+            *column = '\0';
         }
         /* Find key-value delimiter */
         column = strchr(line, ':');
         if(0 == column) /* not found a key-value delimiter*/
-        	continue;
+            continue;
 
+        int attr_id = -1; /* Lookup attribute key */
+        *column++ = 0; /* replace ':' into '\0' and advance */
+
+        /* Find attribute Id by attribute name */
+#ifdef USE_HASH_TABLE
+        /* Hash table lookup: attribute Id by attribute name */
+        if(0 != hash_lookup(attr_name, (bbami_attribute_id*)&attr_id))
+            continue; /* Attribute key was not found */
+
+#else
+        /* Linear search: attribute Id by attribute name */
         int i;
-		int attr_id = -1; /* Lookup attribute key */
-		*column++ = 0; /* replace ':' into '\0' and advance */
+        for(i = 0; i < NUM_ATTRIBUTES; ++i) {
+            if(0 == strcmp(attr_names[i], attr_name)) {
+                attr_id = i;
+                break;
+            }
+        }
 
-		/* Find attribute Id by attribute name */
-		/* TODO: implement using hash */
-		for(i = 0; i < NUM_ATTRIBUTES; ++i) {
-			if(0 == strcmp(attr_names[i].attr_name, attr_name)) {
-				attr_id = i;
-				break;
-			}
-		}
+        if(attr_id < 0) /* Attribute key was not found */
+            continue;
+#endif
 
-		if(attr_id < 0) /* Attribute key was not found */
-			continue;
+        if(0 == *column++) /* empty value - skip */
+            continue;
 
-		if(0 == *column++) /* empty value - skip */
-			continue;
+        char * tmp;
+        size_t sz;
+        struct hash_entry * entry = &info->table[attr_id];
+        value = column;
 
-		char * tmp;
-		size_t sz;
-		struct hash_entry * entry = &info->table[attr_id];
-		value = column;
-
-		switch(entry->count) {
-		case 0:
-			entry->count++;
-			entry->value.item = strdup(value);
-			if(entry->value.item == 0) {
-				fclose(f);
-				return -errno;
-			}
-			break;
-		case 1:
-			entry->count++; /* becomes 2 */
-			tmp = entry->value.item; /* save old single value */
-			/* allocate enough room for 2 items */
-			sz = sizeof(char*) * entry->count;
-			entry->value.item_list = realloc(0, sz);
-			if(entry->value.item_list == 0) {
-				fclose(f);
-				return -errno;
-			}
-			entry->value.item_list[0] = tmp; /* restore old item */
-			entry->value.item_list[1] = strdup(value); /* make a copy of the new one */
-			if(entry->value.item_list == 0) {
-				fclose(f);
-				return -errno;
-			}
-			break;
-		default: /* becomes greater than 2*/
-			entry->count++;
-			/* how much memory for the new list of values */
-			sz = sizeof(char**) * entry->count;
-			/* allocate memory for the new list */
-			/* need to use tmp variable because allocation may fail */
-			tmp = realloc(entry->value.item_list, sz);
-			if(tmp == 0) { /* allocation failed */
-				fclose(f);
-				return -errno;
-			} else {
-				/* succeeded - use tmp variable */
-				entry->value.item_list = (char**)tmp;
-			}
-			/* copy value itself */
-			entry->value.item_list[entry->count - 1] = strdup(value);
-			if(entry->value.item_list[entry->count - 1] == 0) {
-				fclose(f);
-				return -errno;
-			}
-			break;
-		} /* switch */
+        switch(entry->count) {
+        case 0:
+            entry->count++;
+            entry->value.item = strdup(value);
+            if(entry->value.item == 0) {
+                fclose(f);
+                return -errno;
+            }
+            break;
+        case 1:
+            entry->count++; /* becomes 2 */
+            tmp = entry->value.item; /* save old single value */
+            /* allocate enough room for 2 items */
+            sz = sizeof(char*) * entry->count;
+            entry->value.item_list = realloc(0, sz);
+            if(entry->value.item_list == 0) {
+                fclose(f);
+                return -errno;
+            }
+            entry->value.item_list[0] = tmp; /* restore old item */
+            entry->value.item_list[1] = strdup(value); /* make a copy of the new one */
+            if(entry->value.item_list == 0) {
+                fclose(f);
+                return -errno;
+            }
+            break;
+        default: /* becomes greater than 2*/
+            entry->count++;
+            /* how much memory for the new list of values */
+            sz = sizeof(char**) * entry->count;
+            /* allocate memory for the new list */
+            /* need to use tmp variable because allocation may fail */
+            tmp = realloc(entry->value.item_list, sz);
+            if(tmp == 0) { /* allocation failed */
+                fclose(f);
+                return -errno;
+            } else {
+                /* succeeded - use tmp variable */
+                entry->value.item_list = (char**)tmp;
+            }
+            /* copy value itself */
+            entry->value.item_list[entry->count - 1] = strdup(value);
+            if(entry->value.item_list[entry->count - 1] == 0) {
+                fclose(f);
+                return -errno;
+            }
+            break;
+        } /* switch */
     }
 
     fclose(f);
