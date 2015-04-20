d62eac1e539622d23c1b7c46ed2a75e19ad26a1d
Oleg Kosenkov <oleg@kosenkov.ca>
2015-04-19 20:50:15 -0400

    Added x86 variant

--- common.mk
+++ common.mk
@@ -11,10 +11,20 @@ define PINFO
 PINFO DESCRIPTION = BlackBerry Application Meta Information Library
 endef
 
+#===== USEFILE - the file containing the usage message for the application.
 USEFILE=
+
+#===== NAME - name of the project (default - name of project directory).
 NAME=bbami
-EXTRA_INCVPATH +=$(PROJECT_ROOT)/public
-CCFLAGS += -D__BLACKBERRY__ -D__QNXNTO__
+
+#===== EXTRA_INCVPATH - a space-separated list of directories to search for include files.
+EXTRA_INCVPATH+=$(PROJECT_ROOT)/public
+
+#===== CCFLAGS - add the flags to the C compiler command line.
+CCFLAGS+=-D__BLACKBERRY__ -D__QNXNTO__
+
+#===== EXTRA_SILENT_VARIANTS - variants that are not appended to the result binary name (like MyBin_g)
+EXTRA_SILENT_VARIANTS+=x86
 
 include $(MKFILES_ROOT)/qmacros.mk
 ifndef QNX_INTERNAL
@@ -23,5 +33,7 @@ endif
 include $(QNX_INTERNAL)
 
 include $(MKFILES_ROOT)/qtargets.mk
+
 OPTIMIZE_TYPE_g=none
 OPTIMIZE_TYPE=$(OPTIMIZE_TYPE_$(filter g, $(VARIANTS)))
+
